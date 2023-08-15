262552283839a44fcfd388b83640476f31510d14
Unique-Usman <usmanakinyemi202@gmail.com>
2023-08-16 03:06:38 +0530

    a program that imports all functions from the file calculator_1.py and handles basic operations like addition, multiplication, subtraction and division

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, div, mul
+import sys
+
+
+def main():
+    """My calculator"""
+
+    operator = ["+", "-", "*", "/"]
+    size = len(sys.argv) - 1
+    if size != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    if sys.argv[2] not in operator:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    if sys.argv[2] == "+":
+        ans = int(sys.argv[1]) + int(sys.argv[3])
+    elif sys.argv[2] == "-":
+        ans = int(sys.argv[1]) - int(sys.argv[3])
+    elif sys.argv[2] == "/":
+        ans = int(sys.argv[1]) / int(sys.argv[3])
+    else:
+        ans = int(sys.argv[1]) * int(sys.argv[3])
+    print("{} {} {}  = {}".format(sys.argv[1], sys.argv[2],  sys.argv[3], ans))
+
+
+if __name__ == "__main__":
+    main()
