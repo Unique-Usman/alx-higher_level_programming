17902a857983bfcac2c59deea769a3a5fdafad01
Unique-Usman <usmanakinyemi202@gmail.com>
2023-08-17 11:40:43 +0530

    make the function no_c(my_string) to remove all the occurence of c and capital c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,8 +3,8 @@
 
 def no_c(my_string):
     my_string = list(my_string)
-    if "c" in my_string:
+    while "c" in my_string:
         my_string.remove("c")
-    if "C" in my_string:
+    while "C" in my_string:
         my_string.remove("C")
     return "".join(my_string)
