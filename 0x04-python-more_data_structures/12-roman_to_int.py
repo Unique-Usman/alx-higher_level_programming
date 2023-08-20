c16d3d28f8c37956995f5433e75e2b4820c9158d
Unique-Usman <usmanakinyemi202@gmail.com>
2023-08-20 09:37:16 +0530

    resolve the pycodestyle error in 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) != str or roman_string == None:
+    if type(roman_string) is not str or roman_string is None:
         return 0
-    r_n = {
-    'I': 1,
-    'V': 5,
-    'X': 10,
-    'L': 50,
-    'C': 100,
-    'D': 500,
-    'M': 1000
-    }
+    r_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     res = 0
     while roman_string:
         one = roman_string[0]
