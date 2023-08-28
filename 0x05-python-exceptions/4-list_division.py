ec2ef1965d39f11ded699246620765f72a6f232f
Unique-Usman <usmanakinyemi202@gmail.com>
2023-08-29 00:38:20 +0530

    change the pyhton2 to python3

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -9,10 +9,13 @@ def list_division(my_list_1: list, my_list_2: list, list_length: list) -> list:
             ans = my_list_1[i] / my_list_2[i]
         except TypeError:
             print("wrong Type")
+            ans = 0
         except ZeroDivisionError:
             print("division by 0")
+            ans = 0
         except IndexError:
             print("out of range")
+            ans = 0
         finally:
             new_list.append(ans)
     return new_list
