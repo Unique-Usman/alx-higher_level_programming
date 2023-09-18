fc93e79e39a2d4706eeefa18194f608c14238791
Unique-Usman <usmanakinyemi202@gmail.com>
2023-09-18 22:26:45 +0530

    Made the load file to be able to added item into the list on different execution

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,9 +2,15 @@
 """add all arg to list and then save to json file"""
 import sys
 import json
+import os
 
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 data = sys.argv[1:]
+
+if (os.path.exists("add_item.json")):
+    with open("add_item.json", "r", encoding="utf-8") as fd:
+        data = json.load(fd) + data
+
 save_to_json_file(data, "add_item.json")
