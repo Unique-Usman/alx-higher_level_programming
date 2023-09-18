3b20c91714404e72f03f9bf60fd08c9a2ed4aeb8
Unique-Usman <usmanakinyemi202@gmail.com>
2023-09-18 22:48:46 +0530

    A student class

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -14,6 +14,7 @@ class Student:
         """
         self.first_name = first_name
         self.last_name = last_name
+        self.age = age
 
     def to_json(self):
         """change the class to student
