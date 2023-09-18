f746f117d4c4c3acea2790b2fac11b861b752008
Unique-Usman <usmanakinyemi202@gmail.com>
2023-09-18 22:46:40 +0530

    A student class

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""A Student class"""
+
+
+class Student:
+    """A student class"""
+    def __init__(self, first_name, last_name, age):
+        """The init function
+
+        Args:
+            first_name (str): the first_name
+            last_name (str): the last_name
+            age (int): the age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def to_json(self):
+        """change the class to student
+
+        Returns:
+            dict: the dictionary representation of the class
+        """
+        return self.__dict__
