5adceaa02fdfc9137d69bc8945a3ef997d02bc47
Unique-Usman <usmanakinyemi202@gmail.com>
2023-09-20 02:26:31 +0530

    Worked created the student from the json file

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -31,3 +31,16 @@ class Student:
                     res[i] = (self.__dict__)[i]
             return res
         return self.__dict__
+
+    def reload_from_json(self, json):
+        """replaces attribute of the Student
+
+        Args:
+            json (dict): the dictionary object
+        """
+        if "first_name" in json:
+            self.first_name = json["first_name"]
+        if "last_name" in json:
+            self.last_name = json["last_name"]
+        if "age" in json:
+            self.age = json["age"]
