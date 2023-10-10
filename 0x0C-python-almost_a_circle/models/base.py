2e8410576133c47ce97ffe88bb47b193c95444fe
Unique-Usman <usmanakinyemi202@gmail.com>
2023-10-10 23:41:39 +0530

    Added a function that returns the JSON string representation of list_dictionaries:

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Base class"""
-
+import json
 
 class Base:
     """The Base class"""
@@ -17,3 +17,17 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """that returns the JSON string representation of list_dictionaries:
+
+        Args:
+            list_dictionaries (list): List of dictionary
+        Returns:
+            str: The json string representation of the  list_dictionaries(arg)
+        """
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
