e3b2e5917a99ff64dd02637002d186e86ecc4bec
Unique-Usman <usmanakinyemi202@gmail.com>
2023-12-24 12:10:26 +0530

    Completed my Javascript assignment

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,26 +1,39 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
+
+  function convert(arr) {
+    let ans = 0;
+    for (let i = arr.length - 1; i >= 0; i--) {
+      ans = ans * 10 + arr[i]; 
+    }
+    return ans;
+  }
   return function (val) {
-    if (val !== 16) {
-      let ans = 0;
+    if (base !== 16) {
+      let ans = [];
       while (val !== 0) {
-        ans = (ans * 10) + (val % base);
+        ans.push(val % base);
         val = Math.floor(val / base);
       }
 
-      return ans;
+      return convert(ans);
     } else {
       const res = {
-        10: 'a',
-        11: 'c'
-
+        "10": 'a',
+        "11": 'b',
+        "12": 'c',
+        "13": 'd',
+        "14": 'e',
+        "15": 'f'
       };
-      let ans = 0;
+      let ans = "";
       while (val !== 0) {
-        ans = (ans * 10) + (val % base);
+        let temp = val % base;
+        ans = (res[`${temp}`] ? res[`${temp}`]: temp) + ans;
         val = Math.floor(val / base);
       }
+      return ans;
     }
   };
 };
